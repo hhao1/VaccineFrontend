d36e4085b739bd45797e7a3670eeab0623d32163
Hao Hao <hhao1@ualberta.ca>
2020-03-30 07:46:19 -0700

    add check box and set the default address

--- src/components/Layout/Info.js
+++ src/components/Layout/Info.js
@@ -12,7 +12,7 @@ import { getVaccines } from "../../actions/vaccines";
 import { getMarkers } from "../../actions/markers";
 import { getCountryCode } from "../../actions/set_current_country_code";
 
-import "../../css/info.css"
+import "../../css/info.css";
 
 class Info extends Component {
   static propTypes = {
@@ -48,96 +48,120 @@ class Info extends Component {
   }
 
   render() {
-    const {
-      vaccines,
-      markers,
-      curDestination,
-      curCountryObject,
-      customerLocation
-    } = this.props;
+    const { vaccines, markers, curDestination, curCountryObject } = this.props;
+    var curLocation = null;
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        curLocation = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+      });
+    }
+    var { customerLocation } = this.props;
+    if (customerLocation == null && curLocation == null) {
+      customerLocation = {
+        lat: 51.0047330999999,
+        lng: -114.0718831
+      };
+    } else if (customerLocation == null && curLocation != null) {
+      customerLocation = curLocation;
+    }
+
     var curCountryVaccines = new Set(curCountryObject.vaccines);
     return (
       <Jumbotron className="container mt-4 info-page-container">
         {/* <Container> */}
-          {/* Changing the layout to grid to make it responsive to screen size */}
-          <div className="info-container">
+        {/* Changing the layout to grid to make it responsive to screen size */}
+        <div className="info-container">
+          <Row className="info-header">
+            <h3 className="text-secondary">
+              Vaccines you should get before travelling to {curDestination}
+            </h3>
+          </Row>
 
-              <Row className="info-header">
-                <h3 className="text-secondary">
-                  Vaccines you should get before travelling to {curDestination}
-                </h3>
-              </Row>
-
-              <div className="vaccine-list">
-                  <Col className="overflow-auto" style={{ height: "30em" }}>
-                    {vaccines
-                      .filter(
-                        vaccine =>
-                          vaccine.notice !== "" &&
-                          curCountryVaccines.has(vaccine.id)
-                      )
-                      .map(vaccine => (
-                        <CardContainer
-                          name={vaccine.name}
-                          description={vaccine.detail}
-                          key={vaccine.id}
-                          isImportant={true}
-                          notice={vaccine.notice}
-                        />
-                      ))}
-                    {vaccines
-                      .filter(
-                        vaccine =>
-                          vaccine.notice === "" &&
-                          vaccine.name !== "" &&
-                          curCountryVaccines.has(vaccine.id)
-                      )
-                      .map(vaccine => (
-                        <CardContainer
-                          name={vaccine.name}
-                          description={vaccine.detail}
-                          key={vaccine.id}
-                          notice={vaccine.notice}
-                        />
-                      ))}
-                  </Col>
-              </div>
-              {markers.length > 0 && <div className="store-list">
-                  <Map
-                      id="myMap"
-                      options={{
-                        center: customerLocation,
-                        zoom: 8
-                      }}
-                      onMapLoad={map => {
-                        new window.google.maps.Marker({
-                          position: customerLocation,
-                          map: map
-                        });
-
-                        markers.map((l, index) => {
-                          var letter = String.fromCharCode(
-                            "A".charCodeAt(0) + index
-                          );
-                          new window.google.maps.Marker({
-                            position: { lat: Number(l.lat), lng: Number(l.lon) },
-                            icon:
-                              "http://maps.google.com/mapfiles/marker" +
-                              letter +
-                              ".png",
-                            map: map
-                          });
-                        });
-                      }}
+          <div className="vaccine-list">
+            <Col className="overflow-auto" style={{ height: "30em" }}>
+              {vaccines
+                .filter(
+                  vaccine =>
+                    vaccine.notice !== "" && curCountryVaccines.has(vaccine.id)
+                )
+                .map(vaccine => (
+                  <CardContainer
+                    name={vaccine.name}
+                    description={vaccine.detail}
+                    key={vaccine.id}
+                    isImportant={true}
+                    notice={vaccine.notice}
                   />
-
-                  <div className="store-info-list"> 
-                      {markers.map(maker => <StoreInfoCard key={maker.id} name={maker.name} number={maker.phone} address={maker.address} hour={maker.hours}/>)}
-                  </div>
-                
-                </div>
-              }
+                ))}
+              {vaccines
+                .filter(
+                  vaccine =>
+                    vaccine.notice === "" &&
+                    vaccine.name !== "" &&
+                    curCountryVaccines.has(vaccine.id)
+                )
+                .map(vaccine => (
+                  <CardContainer
+                    name={vaccine.name}
+                    description={vaccine.detail}
+                    key={vaccine.id}
+                    notice={vaccine.notice}
+                  />
+                ))}
+            </Col>
           </div>
+          {markers.length > 0 && (
+            <div className="store-list">
+              <Map
+                id="myMap"
+                options={{
+                  center: customerLocation,
+                  zoom: 8
+                }}
+                onMapLoad={map => {
+                  new window.google.maps.Marker({
+                    position: customerLocation,
+                    map: map
+                  });
+
+                  markers.map((l, index) => {
+                    var letter = String.fromCharCode("A".charCodeAt(0) + index);
+                    new window.google.maps.Marker({
+                      position: { lat: Number(l.lat), lng: Number(l.lon) },
+                      icon:
+                        "http://maps.google.com/mapfiles/marker" +
+                        letter +
+                        ".png",
+                      map: map
+                    });
+                  });
+                }}
+              />
+
+              <div className="store-info-list">
+                {markers.map(maker => (
+                  <StoreInfoCard
+                    key={maker.id}
+                    name={maker.name}
+                    number={maker.phone}
+                    address={maker.address}
+                    hour={maker.hours}
+                    injection_certified={maker.injection_certified}
+                    certified_travel_consultant={
+                      maker.certified_travel_consultant
+                    }
+                    additional_prescribing_authority={
+                      maker.additional_prescribing_authority
+                    }
+                  />
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
         {/* </Container> */}
       </Jumbotron>
     );
--- src/components/Layout/StoreInfoCard.js
+++ src/components/Layout/StoreInfoCard.js
@@ -1,41 +1,67 @@
 import React, { Component } from "react";
-import { Card, CardTitle, CardText } from "reactstrap";
+import { Card, CardTitle, CardText, FormGroup, Label, Input } from "reactstrap";
 import Info from "./Info";
 
 class StoreInfoCard extends Component {
+  render() {
+    const {
+      name,
+      number,
+      hour,
+      address,
+      injection_certified,
+      certified_travel_consultant,
+      additional_prescribing_authority
+    } = this.props;
 
-    render(){
+    const openhour = hour == null ? "/" : hour;
 
-        const {
-            name,
-            number,
-            hour,
-            address
-        } = this.props;
+    const hourStyle = {
+      color: hour == null ? "rgb(234,236, 239)" : "gray",
+      display: "inline"
+    };
 
-        const openhour = hour==null? "/" : hour;
+    const generalStyle = {
+      color: "gray",
+      display: "inline"
+    };
 
-        const hourStyle = {
-            color: hour==null? "rgb(234,236, 239)" : "gray",
-            display: "inline"
-        }
-
-        const generalStyle = {
-            color: "gray",
-            display: "inline"
-        }
-
-        return(
-
-            <Card body>
-                <h3>{name}</h3>
-                <h4>Phone: <p style={generalStyle}>{number}</p></h4>
-                <h4>Open Hours: <p style={hourStyle}>{openhour}</p></h4>
-                <h4>Address: <p style={generalStyle}>{address}</p></h4>
-            </Card>
-        )
-    }
+    return (
+      <Card body>
+        <h3>{name}</h3>
+        <h4>
+          Phone: <p style={generalStyle}>{number}</p>
+        </h4>
+        <h4>
+          Address: <p style={generalStyle}>{address}</p>
+        </h4>
+        <FormGroup check disabled>
+          <Label check>
+            <Input type="checkbox" defaultChecked={injection_certified} />{" "}
+            Injection Certified
+          </Label>
+        </FormGroup>
+        <FormGroup check disabled>
+          <Label check>
+            <Input
+              type="checkbox"
+              defaultChecked={certified_travel_consultant}
+            />{" "}
+            Certified Travel Consultant
+          </Label>
+        </FormGroup>
+        <FormGroup check disabled>
+          <Label check>
+            <Input
+              type="checkbox"
+              defaultChecked={additional_prescribing_authority}
+            />{" "}
+            Additional Prescribing Authority
+          </Label>
+        </FormGroup>
+      </Card>
+    );
+  }
 }
 
 export default StoreInfoCard;
-  
