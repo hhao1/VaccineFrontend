8d8b5180d0d4e417641aeec9150438da6ab16f6b
Hao Hao <hhao1@ualberta.ca>
2020-03-31 12:14:11 -0700

    update

--- src/actions/markers.js
+++ src/actions/markers.js
@@ -5,7 +5,7 @@ import { GET_MARKERS } from "./types";
 //GET MARKERS
 export const getMarkers = () => dispatch => {
   axios
-    .get("https://database.vaccinefinders.ca/api//makers/") // Should change to /api/markers
+    .get("https://database.vaccinefinders.ca/api/makers/") // Should change to /api/markers
     .then(res => {
       dispatch({
         type: GET_MARKERS,
